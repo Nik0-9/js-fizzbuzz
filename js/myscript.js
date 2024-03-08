7d9dfed3d8060a7e6d7228ddf0126d5a4407878a
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-08 16:50:05 +0100

    add in tmpHtml all element that i need

--- js/myscript.js
+++ js/myscript.js
@@ -6,33 +6,39 @@ Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un ele
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
 */
-// let squareWrapper = document.createElement('div');
-// squareWrapper.className = 'd-flex justify-content-between align-content-center flex-wrap gap-2';
-// squareWrapper.setAttribute('id', 'content');
+//squareWriter crea un elemento nel documento
+const squareWraiter = document.createElement('div'); 
+//squareWriter.className aggiunge classi all'elemento creato
+squareWraiter.className = 'd-flex justify-content-between align-content-center flex-wrap gap-2';
+//squareWriter.setAttribute aggiunge attributi all'elemento creato
+squareWraiter.setAttribute('id', 'content');
 
 let numBox = 100;
 let tmpHtml = '';
-let num = 0
+let num = 0;
 
-for(let i = 1; i < numBox; i++){
-    if((i % 3 === 0) && (i % 5 === 0)){
+for(let i = 1; i <= numBox; i++){
+    if(i %15 === 0){
+        tmpHtml += `<div class="box fizz-buzz"> fizz buzz </div>`;
         num = 'fizzbuzz';
         console.log(num);
     }
-    else if(i % 5 ===0){
+    else if(i % 5 === 0){
+        tmpHtml += `<div class="box buzz"> buzz </div>`;
         num = 'buzz';
         console.log(num);
     }else if(i % 3 === 0){
-        // tmpHtml += `<div class="box fizz"> fizz </div>`;
-         num = 'fizz'
-         console.log(num)
+        tmpHtml += `<div class="box fizz"> fizz </div>`;
+        num = 'fizz';
+        console.log(num)
      }
      else{
-        // tmpHtml += `<div class="box"> ${i + 1} </div>`;
+        tmpHtml += `<div class="box"> ${i} </div>`;
         num = i;
         console.log(num);
     }
 }
-// squareWrapper.innerHTML = tmpHtml;
+console.log(squareWraiter);
+squareWraiter.innerHTML = tmpHtml;
 // let container = document.querySelector('container');
-// container.append(squareWrapper);
+// container.append(squareWraiter);
